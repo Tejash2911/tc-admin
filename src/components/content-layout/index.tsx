66f11c2a46559f18c9ca2073b65fdd0bbd7a94f9
Tejash2911 <tcpatel2911@gmail.com>
2025-06-13 23:13:22 +0530

    remove: className prop

--- src/components/content-layout/index.tsx
+++ src/components/content-layout/index.tsx
@@ -3,12 +3,11 @@ import { FC } from 'react'
 interface ContentLayoutProps {
   children: React.ReactNode
   title?: string
-  className?: string
 }
 
-const ContentLayout: FC<ContentLayoutProps> = ({ children, title = 'Content', className = '' }) => {
+const ContentLayout: FC<ContentLayoutProps> = ({ children, title = 'Content' }) => {
   return (
-    <div className={`min-h-[calc(100vh-50px)] w-full flex flex-col items-center box-border ${className}`}>
+    <div className={`min-h-[calc(100vh-50px)] w-full flex flex-col items-center box-border`}>
       <div className='w-full max-w-7xl px-4 sm:px-6 lg:px-8'>
         <h1 className='text-xl font-bold mb-4 text-center mt-4'>{title}</h1>
         {children}
